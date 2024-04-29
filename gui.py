90e9e3b8808a88f11c1360f20c5fe486f75c4415
fiveoverthree <64463394+fiveoverthree@users.noreply.github.com>
2024-04-29 13:44:09 +0200

    bettert layout

--- gui.py
+++ gui.py
@@ -19,11 +19,14 @@ import pyperclip
 class App(TKMT.ThemedTKinterFrame):
     def __init__(self):
         super().__init__("LTspice to Latex converter", "park", "dark")
+        self.root.geometry("+0+0")
         #self.button_frame = self.addLabelFrame("Frame Label")
-        self.Button("Open file", self.load) #the button is dropped straight into the frame
+        btn = self.Button("Open file", self.load) #the button is dropped straight into the frame
+        btn.grid(column=0, row=1)
         self.run()
         self.text = None
         self.text_area = None
+        
 
     def load(self):
         #print("Button clicked!")
@@ -37,13 +40,13 @@ class App(TKMT.ThemedTKinterFrame):
 
     def updateText(self):
         self.text_area = ScrolledText(self.root, wrap=tk.WORD, width=100, height=30)
-        self.text_area.grid(column=0, row=1, padx=5, pady=5)
+        self.text_area.grid(column=0, row=2, padx=5, pady=5)
         self.text_area.delete('1.0', tk.END)   # Clear existing text
         self.text_area.insert(tk.END, self.text)    # Insert new text
 
     def drawCopyButton(self):
         copy_button = self.AccentButton("Copy to Clipboard", self.copy_to_clipboard)
-        copy_button.grid(column=0, row=2, padx=5, pady=5)
+        copy_button.grid(column=0, row=0, padx=5, pady=5)
 
     def copy_to_clipboard(self):        
         text_to_copy = self.text_area.get("1.0", "end-1c")  # Get text from text area
